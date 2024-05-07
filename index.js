14b3d9a6110e8ab044da0107a408435e5ab66ca5
tungoiii34 <julita1404pun0suy@hotmail.com>
2024-05-07 22:38:32 +0700

    demo task

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function canCross(stones) {
+  const map = new Map();
+  for (const stone of stones) map.set(stone, new Set());
+  map.get(0).add(0);
+  for (let i = 0; i < stones.length; i++) {
+    for (const step of map.get(stones[i])) {
+      for (let k = step - 1; k <= step + 1; k++) {
+        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+      }
+    }
+  }
+  return map.get(stones[stones.length - 1]).size > 0;
+}
