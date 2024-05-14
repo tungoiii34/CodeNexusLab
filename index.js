e69dee60e452ba4b85df70a363f729113393c03b
tungoiii34 <julita1404pun0suy@hotmail.com>
2024-05-14 11:42:26 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
+const factorialRecursive = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  return dp[0];
-}
+  return n * factorialRecursive(n - 1);
+};
